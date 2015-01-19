75453da9cd4d04e011838dbb2c32e5ff20fdaeda
Juan Carlos Orrego <jcorrego@gmail.com>
2015-01-19 18:59:46 -0500

    Mapas de google

--- sites/all/modules/custom/buscador/js/googlemaps.js
+++ sites/all/modules/custom/buscador/js/googlemaps.js
@@ -1,23 +1,18 @@
 /**
  * Created by jcorrego on 19/01/15.
  */
+var map;
 function initialize() {
     var mapOptions = {
         center: new google.maps.LatLng(-34.397, 150.644),
         zoom: 8,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    var map = new google.maps.Map(document.getElementById("mapa"),
+    map = new google.maps.Map(document.getElementById("mapa"),
         mapOptions);
 }
 function initialize2() {
-    var mapOptions = {
-        center: new google.maps.LatLng(3.427581, -76.497675),
-        zoom: 8,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var map = new google.maps.Map(document.getElementById("mapa"),
-        mapOptions);
+    map.panToBounds({ sw:{lat:3.427581, lng:-76.49767}, ne:{lat:4.565961, lng:-74.078279} });
 }
 jQuery(document).ready( function(){
     initialize();
