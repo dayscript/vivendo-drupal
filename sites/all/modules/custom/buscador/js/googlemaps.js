df49e150055f8ebb15491c1c230e9655a2abb8e3
Juan Carlos Orrego <jcorrego@gmail.com>
2015-01-19 20:33:32 -0500

    Mejoras en el mapa

--- sites/all/modules/custom/buscador/js/googlemaps.js
+++ sites/all/modules/custom/buscador/js/googlemaps.js
@@ -38,7 +38,8 @@ function update( ) {
             var marker = new google.maps.Marker({
                 position: points[i].latlng,
                 map: map,
-                title: points[i].name
+                title: points[i].name,
+                icon: '/sites/all/themes/at-vivendo/images/icons/marker.png'
             });
             gmarkers.push(marker);
             latlngbounds.extend(points[i].latlng);
