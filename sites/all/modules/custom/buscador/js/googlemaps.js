eecbedc41108e8c50d6f573833b54c52da08299a
Juan Carlos Orrego <jcorrego@gmail.com>
2015-01-19 20:01:32 -0500

    Mostrar puntos en el mapa

--- sites/all/modules/custom/buscador/js/googlemaps.js
+++ sites/all/modules/custom/buscador/js/googlemaps.js
@@ -22,27 +22,20 @@ function initialize() {
         map.fitBounds(latlngbounds);
     }
 }
-function update( points ) {
-    var latlngbounds = new google.maps.LatLngBounds();
-    var points = [
-        {
-            name: "Punto 1",
-            latlng: new google.maps.LatLng(4.565961, -74.078279)
-        },
-        {
-            name: "Punto 2",
-            latlng: new google.maps.LatLng(3.427581,-76.49767)
+function update( ) {
+    if(points && points.length){
+        var latlngbounds = new google.maps.LatLngBounds();
+        for (var i = 0; i < points.length; i++) {
+            new google.maps.Marker({
+                position: points[i].latlng,
+                map: map,
+                title: points[i].name
+            });
+            latlngbounds.extend(points[i].latlng);
         }
-    ];
-    for (var i = 0; i < points.length; i++) {
-        new google.maps.Marker({
-            position: points[i].latlng,
-            map: map,
-            title: points[i].name
-        });
-        latlngbounds.extend(points[i].latlng);
+        map.fitBounds(latlngbounds);
     }
-    map.fitBounds(latlngbounds);
+
 }
 jQuery(document).ready( function(){
     initialize();
