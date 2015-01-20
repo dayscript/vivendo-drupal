f41356b89dd3eee7ba2d35e94755524b38db3b45
Juan Carlos Orrego <jcorrego@gmail.com>
2015-01-19 20:14:58 -0500

    Mostrar puntos en el mapa

--- sites/all/modules/custom/buscador/js/googlemaps.js
+++ sites/all/modules/custom/buscador/js/googlemaps.js
@@ -2,6 +2,7 @@
  * Created by jcorrego on 19/01/15.
  */
 var map;
+var gmarkers = [];
 function initialize() {
     var mapOptions = {
         center: new google.maps.LatLng(4.565961, -74.078279),
@@ -12,25 +13,33 @@ function initialize() {
     if(points && points.length){
         var latlngbounds = new google.maps.LatLngBounds();
         for (var i = 0; i < points.length; i++) {
-            new google.maps.Marker({
+            var marker = new google.maps.Marker({
                 position: points[i].latlng,
                 map: map,
                 title: points[i].name
             });
+            gmarkers.push(marker);
             latlngbounds.extend(points[i].latlng);
         }
         map.fitBounds(latlngbounds);
     }
 }
+function removeMarkers(){
+    for(i=0; i<gmarkers.length; i++){
+        gmarkers[i].setMap(null);
+    }
+}
 function update( ) {
     if(points && points.length){
         var latlngbounds = new google.maps.LatLngBounds();
+        removeMarkers();
         for (var i = 0; i < points.length; i++) {
-            new google.maps.Marker({
+            var marker = new google.maps.Marker({
                 position: points[i].latlng,
                 map: map,
                 title: points[i].name
             });
+            gmarkers.push(marker);
             latlngbounds.extend(points[i].latlng);
         }
         map.fitBounds(latlngbounds);
