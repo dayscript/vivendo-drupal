4acfc77d1013bffe68af5c8785df8a7dc6d1fbfd
Fabian Esteban Torres Romero <drivas@dayscript.com>
2015-01-22 14:18:52 -0500

    changes in google map script

--- sites/all/modules/custom/buscador/js/googlemaps.js
+++ sites/all/modules/custom/buscador/js/googlemaps.js
@@ -15,7 +15,7 @@ function initialize() {
     map = new google.maps.Map(document.getElementById("mapa"), mapOptions);
     
   if(points && points.length){
-        var latlngbounds = new google.maps.LatLngBounds();
+        //var latlngbounds = new google.maps.LatLngBounds();
         
         var infowindow = new google.maps.InfoWindow({
             content: ''
@@ -27,18 +27,18 @@ function initialize() {
                 position: points[i].latlng,
                 map: map,
                 title: points[i].name,
-                icon: '/sites/all/themes/at-vivendo/images/icons/marker.png'
+                //icon: '/sites/all/themes/at-vivendo/images/icons/marker.png'
             });
             
             google.maps.event.addListener(marker, 'click', function(){
-              infowindow.setContent('<strong>vivendo '+ i.toString() + '</strong>');
+              infowindow.setContent('<strong>vivendo</strong>');
               infowindow.open( map, marker );
             });
             
-            gmarkers.push(marker);
-            latlngbounds.extend(points[i].latlng);
+            //gmarkers.push(marker);
+            //latlngbounds.extend(points[i].latlng);
         }
-        map.fitBounds(latlngbounds);
+       //map.fitBounds(latlngbounds);
     }
 }
 function removeMarkers(){
