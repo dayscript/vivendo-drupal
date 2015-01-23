212e4da19024abc066eb5d78f8cfcc904d27c0ba
Juan Carlos Orrego <jcorrego@gmail.com>
2015-01-22 22:28:26 -0500

    Arreglo de zoom en el mapa

--- sites/all/modules/custom/buscador/js/googlemaps.js
+++ sites/all/modules/custom/buscador/js/googlemaps.js
@@ -41,7 +41,7 @@ function initialize() {
             latlngbounds.extend(points[i].latlng);
         }
        map.fitBounds(latlngbounds);
-       map.setZoom(map.getZoom()-1);
+       map.setZoom(map.getZoom()-2);
     }
 }
 
