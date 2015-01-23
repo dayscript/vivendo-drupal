d0edd31e600ba0dfc704a4a81004fb88f77452aa
Fabian Esteban Torres Romero <drivas@dayscript.com>
2015-01-23 15:50:53 -0500

    adding links for projects

--- sites/all/themes/at-vivendo/scripts/global.js
+++ sites/all/themes/at-vivendo/scripts/global.js
@@ -2,8 +2,9 @@
   
   $(document).ready(function(){
     
-    $('.show-telephone').click(function(){
+    $('.show-telephone').click(function(event){
       $('.hide').show();
+      event.preventDefault();
     });
     
   });
