847bdf45d7efddb8919005975709e912f46ed722
Fabian Esteban Torres Romero <drivas@dayscript.com>
2015-01-26 12:00:40 -0500

    add styles for button up-down map

--- sites/all/themes/at-vivendo/scripts/global.js
+++ sites/all/themes/at-vivendo/scripts/global.js
@@ -9,7 +9,7 @@
     
     $('.b-up').click(function(event){
       event.preventDefault();
-      $('.highlighted-wrapper').css({overflow: 'hidden'}).animate({height: "115px"}, 1000);
+      $('#highlighted-wrapper').css({overflow: 'hidden'}).animate({height: "115px"}, 1000);
       $('#mapa').animate({height: "50px"}, 1000);
     });
     
