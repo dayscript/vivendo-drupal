1f251418980d70ed8807cca57380524fb53a565f
Fabian Esteban Torres Romero <drivas@dayscript.com>
2015-01-26 13:07:59 -0500

    animation map

--- sites/all/themes/at-vivendo/scripts/global.js
+++ sites/all/themes/at-vivendo/scripts/global.js
@@ -7,7 +7,32 @@
       $('.hide').show();
     });
     
-    $('.b-up').click(function(event){
+    
+    $('.up-down a').on('click', function(event){
+      
+      var button = $(this);
+          
+          if ( button.hasClass('b-up') ) {
+            
+            $('.front .view-proyectos-destacados').hide();
+            $('#highlighted-wrapper').css({overflow: 'hidden'}).animate({height: "115px"}, 700);
+            $('#mapa').animate({height: "59px"}, 700);
+            button.removeClass().addClass('b-down');
+            
+          } else if ( button.hasClass('b-down') ) {
+            
+            $('.front .view-proyectos-destacados').show();
+            $('#highlighted-wrapper').css({overflow: 'hidden'}).animate({height: "455px"}, 700);
+            $('#mapa').animate({height: "400px"}, 700);
+            button.removeClass().addClass('b-up');
+            
+          }
+          
+          event.preventDefault();
+      
+    });
+    
+    /*$('.b-up').click(function(event){
       event.preventDefault();
       $('.front .view-proyectos-destacados').hide();
       $('#highlighted-wrapper').css({overflow: 'hidden'}).animate({height: "115px"}, 700);
@@ -21,7 +46,7 @@
       $('#highlighted-wrapper').css({overflow: 'hidden'}).animate({height: "455px"}, 700);
       $('#mapa').animate({height: "400px"}, 700);
       $(this).removeClass().addClass('b-up');
-    });
+    });*/
     
   });
   
