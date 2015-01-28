cca3e660a84145ea8b6e32774625f17c6fd55a9b
Fabian Esteban Torres Romero <drivas@dayscript.com>
2015-01-28 10:08:41 -0500

    styles buttons compare

--- sites/all/themes/at-vivendo/scripts/global.js
+++ sites/all/themes/at-vivendo/scripts/global.js
@@ -36,21 +36,9 @@
       
     });
     
-    /*$('.b-up').click(function(event){
+    $('a.link-compare.disable').click(function(event){
       event.preventDefault();
-      $('.front .view-proyectos-destacados').hide();
-      $('#highlighted-wrapper').css({overflow: 'hidden'}).animate({height: "115px"}, 700);
-      $('#mapa').animate({height: "59px"}, 700);
-      $(this).removeClass().addClass('b-down');
     });
-    
-    $('.b-down').click(function(event){
-      event.preventDefault();
-      $('.front .view-proyectos-destacados').hide();
-      $('#highlighted-wrapper').css({overflow: 'hidden'}).animate({height: "455px"}, 700);
-      $('#mapa').animate({height: "400px"}, 700);
-      $(this).removeClass().addClass('b-up');
-    });*/
     
   });
   
